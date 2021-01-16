bc87d99fd2c9900d4f7ebd10ff740c2f4357c6b1
Ola Nguyen Van <olanguyenvan@gmail.com>
2021-01-16 23:39:22 +0100

    feat(Day10): Add solution for day 10

--- src/days/day10/adapters.js
+++ src/days/day10/adapters.js
@@ -1,16 +1,20 @@
-export function findAdaptersNumber(joltages) {
-    joltages.sort((a, b) => a - b);
+export function findAdaptersNumber(adapters) {
+    adapters.sort((a, b) => a - b);
 
     let tmpJoltage = 0;
     let differencesBy1 = 0;
+    let differencesBy2 = 0;
     let differencesBy3 = 0;
 
-    for (let i = 0; i < joltages.length; i++) {
-        const value = joltages[i];
+    for (let i = 0; i < adapters.length; i++) {
+        const value = adapters[i];
 
         switch (value - tmpJoltage) {
             case 1:
                 differencesBy1 += 1;
+                break;
+            case 2:
+                differencesBy2 += 1;
                 break;
             case 3:
                 differencesBy3 += 1;
@@ -22,6 +26,34 @@ export function findAdaptersNumber(joltages) {
 
     return {
         differencesBy1,
+        differencesBy2,
         differencesBy3: differencesBy3 + 1,
     };
+}
+
+const VALID_ADAPTERS = [1, 2, 3];
+
+export function findAllAdaptersCombinations(adapters) {
+    adapters.unshift(0);
+    adapters.sort((a, b) => a - b);
+
+    const adaptersCount = adapters.length;
+    const adaptersCombinations = new Array(adaptersCount);
+    adaptersCombinations.fill(0);
+    adaptersCombinations[adaptersCount - 1] = 1;
+
+    for (let i = adaptersCount - 1; i >= 0; i--) {
+        const value = adapters[i];
+
+        for (let j = 1; j <= 3; j++) {
+            const neighbourIndex = i - j;
+            const neighbourValue = adapters[neighbourIndex];
+
+            if (VALID_ADAPTERS.includes(value - neighbourValue)) {
+                adaptersCombinations[neighbourIndex] += adaptersCombinations[i];
+            }
+        }
+    }
+
+    return adaptersCombinations[0];
 }
